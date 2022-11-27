093c9fd9257e466cc3123a11c2e120015a9260d5
Kamilly Quadros <102553358+virtualkami@users.noreply.github.com>
2022-11-27 14:01:08 -0400

    Update Program.cs

--- Atividade1_27_11/Program.cs
+++ Atividade1_27_11/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Atividade1_27_11
+namespace Atividade1_27_11
 {
     internal class Program
     {
@@ -9,8 +9,10 @@
             Esta é a linha 2
             A penúltima é a linha 3
             A última é a linha 4";
-            // bloco using recebendo e lendo o conteúdo da string linhas e criação da string item
-            using (var leitor = new StringReader(linhas)){ string item;
+            // criação da string item
+            string item;
+            // bloco using recebendo e lendo o conteúdo da string linhas
+            using (var leitor = new StringReader(linhas)){ 
             // bloco do while e item recebendo leitor
                 do{item = leitor.ReadLine();
             // escrever linhas
